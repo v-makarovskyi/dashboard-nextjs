0eb62669ee44a35e631663e789fc7f176a699d4e
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-07-06 15:27:56 +0300

    added route prisma us

---
+++ src/app/seed/route.ts
@@ -0,0 +1,67 @@
+import prisma from "../db";
+import bcrypt from "bcrypt";
+import { users, revenue, customers, invoices } from "../lib/placeholder-data";
+
+async function seedUser() {
+  const insertedUser = await Promise.all(
+    users.map(async (u) => {
+      const hashedPassword = await bcrypt.hash(u.password, 10);
+      return await prisma.user.create({
+        data: {
+          name: u.name,
+          email: u.email,
+          password: hashedPassword,
+        },
+      });
+    })
+  );
+  return insertedUser;
+}
+
+async function seedCustomers() {
+  const insrtedCustomers = await Promise.all(
+    customers.map(async (cust) => {
+      return await prisma.customer.create({
+        data: {
+          name: cust.name,
+          email: cust.email,
+          image_url: cust.image_url,
+          invoices: {
+            create: invoices.map((inv) => {
+              return {
+                amount: inv.amount,
+                status: inv.status,
+              };
+            }),
+          },
+        },
+      });
+    })
+  );
+  return insrtedCustomers;
+}
+
+async function seedRevenue() {
+  const insertedRevenue = await Promise.all(
+    revenue.map(async (r) => {
+      return await prisma.revenue.create({
+        data: {
+          month: r.month,
+          revenue: r.revenue,
+        },
+      });
+    })
+  );
+  return insertedRevenue;
+}
+
+export async function GET() {
+  try {
+    //await seedUser()
+    //await seedCustomers();
+    //await seedRevenue()
+    return Response.json({ message: "Database seeded successfully" });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
