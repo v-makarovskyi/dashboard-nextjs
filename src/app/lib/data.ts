0329402f11666f1c68da58d68d044fd015036052
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-07-06 15:28:35 +0300

    added fetchLatestInvoices fn

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,14 +1,28 @@
 import { PrismaClient } from "@prisma/client";
-import { Revenue } from "./definitions";
-
-const prisma = new PrismaClient()
+import { Revenue, LatestInvoice } from "./definitions";
+import prisma from "../db";
 
 export async function fetchRevenue() {
-    try {
-        const data:Revenue[] = await prisma.revenue.findMany()
-        return data
-    } catch (error) {
-        console.error('DataBase error:', error)
-        throw new Error('Failed to fetch revenue data!')
-    }
+  try {
+    const data: Revenue[] = await prisma.revenue.findMany();
+    return data;
+  } catch (error) {
+    console.error("DataBase error:", error);
+    throw new Error("Failed to fetch revenue data!");
+  }
+}
+
+export async function fetchLatestInvoices() {
+  try {
+    const data: LatestInvoice[] = await prisma.invoice.findMany({
+      select: {
+        amount: true,
+        owner: true
+      }
+    });
+    return data;
+  } catch (error) {
+    console.error("Database error:", error);
+    throw new Error("Failed to fetch invoices data");
+  }
 }
