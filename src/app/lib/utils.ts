c907293ae68b65c21087e5e4376dd3d496c10b28
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-07-06 15:25:29 +0300

    updated

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,10 +1,10 @@
 import { Revenue } from "./definitions"
 
-export function generateYAxis(revenue: Revenue[]) {
+export const generateYAxis = (revenue: Revenue[]) => {
     const yLabelsAxis = []
     const highestRecord = Math.max(...revenue.map((r) => r.revenue))
     const topLabel = Math.ceil(highestRecord / 1000) * 1000
-    for(let i = highestRecord; i <= 0; i -= 1000) {
+    for(let i = topLabel; i >= 0; i -= 1000) {
         yLabelsAxis.push(`$${i / 1000}K`)
     }
     return { yLabelsAxis, topLabel }
