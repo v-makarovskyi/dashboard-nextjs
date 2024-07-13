91225120483ecc75b9c19ea32185cccccd22c4c9
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-07-13 19:15:27 +0300

    added format pagination

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -18,13 +18,33 @@ export const formatCurrency = (amount: number) => {
 };
 
 export const formatDate = (dateStr: string, locale: string = "en-US") => {
-  const date = new Date(dateStr)
-  const dateOptions:Intl.DateTimeFormatOptions = {
-    day: '2-digit',
-    month: 'short',
-    year:'2-digit'
-  }
-  const formatter = new Intl.DateTimeFormat(locale, dateOptions)
-  return formatter.format(date)
+  const date = new Date(dateStr);
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "short",
+    year: "2-digit",
+  };
+  const formatter = new Intl.DateTimeFormat(locale, dateOptions);
+  return formatter.format(date);
 };
 
+export function generatePaginaton(currentPage: number, totalPages: number) {
+  if (totalPages <= 7) {
+    return Array.from({ length: totalPages }, (_, idx) => idx + 1);
+  }
+  if (currentPage <= 3) {
+    return [1, 2, 3, "...", totalPages - 1, totalPages];
+  }
+  if (currentPage >= totalPages - 2) {
+    return [1, 2, "...", totalPages - 2, totalPages - 1, totalPages];
+  }
+  return [
+    1,
+    "...",
+    currentPage - 1,
+    currentPage,
+    currentPage + 1,
+    "...",
+    totalPages,
+  ];
+}
