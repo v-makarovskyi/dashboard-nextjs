6fa45f5d081575f4bd7910d2849f894b6388acb1
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-07-24 19:59:35 +0300

    updated

--- src/app/ui/button.tsx
+++ src/app/ui/button.tsx
@@ -10,7 +10,7 @@ export function Button({ children, className, ...rest }: ButtonProps) {
     <button
       {...rest}
       className={clsx(
-        "h-10 px-4 flex items-center rounded-lg bg-blue-500 text-sm text-white font-medium transition-colors hover:bg-blue-400 focus-visible:outline-blue-600 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
+        "h-10 px-4 flex items-center rounded-lg bg-green-500 text-sm text-white font-medium transition-colors hover:bg-green-400 outline-none focus-visible:outline-green-600 active:bg-green-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
         className
       )}
     >
