dc9b844212ab8f97e21c4c971281047474f62f9d
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-07-24 19:58:23 +0300

    updated

--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from "../acme-logo";
 import Link from "next/link";
 import NavLinks from "./nav-links";
 import { PowerIcon } from "@heroicons/react/24/outline";
+import { signOut } from "../../../../auth";
 
 export default function SideNav() {
   return (
@@ -17,10 +18,13 @@ export default function SideNav() {
       <div className="flex grow flex-row md:flex-col justify-between space-x-2 md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden md:block h-auto grow bg-gray-50 rounded-md"></div>
-        <form>
+        <form action={async () => {
+          'use server'
+          await signOut({redirectTo:'/'})
+        }}>
           <button className="w-full p-3 md:px-3 md:p-2 h-[48px] flex flex-row items-center justify-center md:justify-start gap-3 font-medium text-sm md:bg-gray-50 md:hover:bg-blue-100 transition-colors rounded-md ">
             <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign In</div>
+            <div className="hidden md:block">Sign Out</div>
           </button>
         </form>
       </div>
